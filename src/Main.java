ee08a3b45d72fdbbe6aa7664e489e01ffae17d4d
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-24 18:22:21 +0300

    Методы объектов

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Author pyshkin = new Author("Александр", "");
+        Author tolstoy = new Author("Лев", "Толстой");
+        book prorok = new book("Пророк", pyshkin, 1828);
+        book voina = new book("Война и Мир", tolstoy, 1869);
+
+
+        System.out.println(prorok.equals(voina));
+        System.out.println(voina);
+        System.out.println(prorok);
+        System.out.println(prorok + " " + voina);
+        System.out.println(prorok.hashCode());
+        System.out.println(voina.hashCode());
+    }
+}
---
+++ src/book.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class book {
+   private String name;
+   private Author author;
+   private int year;
+
+    public book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public Author getAuthor() {
+        return this.author;
+    }
+    public int getYear() {
+        return this.year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + name + ", автор: " + author + ", год издания: " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        book book = (book) o;
+        return Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author);
+    }
+}
